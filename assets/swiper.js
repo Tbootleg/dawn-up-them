b751fc561e09ef0c4680b81d794d0b3c3294e312
Thomas James <thomasjames@Thomass-Mac-Studio.local>
2023-02-09 15:19:54 -0600

    adds styles to brand slider

--- assets/swiper.js
+++ assets/swiper.js
@@ -1,8 +1,12 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
+  speed: 400,
+  spaceBetween: 100,
   direction: 'horizontal',
   slidesPerView:8,
   loop: true,
+  setWrapperSize: true,
+  autoHeight: true,
 
   // If we need pagination
   pagination: {
